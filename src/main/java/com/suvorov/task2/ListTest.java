43d854dc960889d4bb7d1492f4120b9f38bebf6c
Paul <suvorovkot@gmail.com>
2018-10-09 04:44:51 +0300

    Queue added

--- src/main/java/com/suvorov/task2/ListTest.java
+++ src/main/java/com/suvorov/task2/ListTest.java
@@ -21,6 +21,17 @@ class ListTest {
     }
 
     @Test
+    void addingInPositionTest() {
+        MyLinkedList<Integer> testList = new MyLinkedList<>();
+        Integer[] begining = {1, 2};
+        testList.create(begining);
+        MyLinkedList<Integer> expectedList = new MyLinkedList<>();
+        testList.addPos(3,1);
+        System.out.println(testList.get(1));
+        //assertEquals(testList.get(1), expectedList.get(1));
+    }
+
+    @Test
     void deletingTest() {
         MyLinkedList<Integer> testList = new MyLinkedList<>();
         Integer[] numbers = {9, 3, 2, 6, 5, 5};
@@ -45,22 +56,14 @@ class ListTest {
     }
 
     @Test
-    void removeExceptionTest() {
+    void gettingTest() {
         MyLinkedList<Integer> testList = new MyLinkedList<>();
-        for (int i = 1; i < 6; ++i)
-            testList.addLast(i);
+        Integer[] numbers = {9, 3, 2, 6, 5, 5};
+        for (int i = 0; i < 6; i++) {
+            testList.addLast(numbers[i]);
+        }
 
-        Integer[] testingArray = new Integer[5];
-        Integer[] indexes = {2, 0, 2, 0, 1};
-
-
-        Throwable exception = assertThrows(NoSuchElementException.class, () -> {
-            int j = 0;
-            for (int i : indexes) {
-                testingArray[j] = testList.deleteByIndex(i);
-                ++j;
-            }
-        });
+        Throwable exception = assertThrows(NoSuchElementException.class, () -> {testList.get(10);});
         assertEquals("Out of bounds", exception.getMessage());
     }
 }
